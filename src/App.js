6bfe5e262985575d7490eafc9d8702ce8c26aee0
Jamshidkhuja Burikhujaev <j.burikhujaev@itransition.com>
2022-10-19 10:50:54 +0500

    feat: change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Angular
+          Learn React
         </a>
       </header>
     </div>
