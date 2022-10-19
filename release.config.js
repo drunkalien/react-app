501a8fe03c054fc52f9c10dac6f3010b32d0e819
Jamshidkhuja Burikhujaev <j.burikhujaev@itransition.com>
2022-10-19 08:25:12 +0500

    fix: fix description

---
+++ release.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  branches: 'master',
+  repositoryUrl: 'https://github.com/drunkalien/react-app',
+  plygins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github',
+  ],
+};
