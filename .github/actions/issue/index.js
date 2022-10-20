12f89a904dd48e94ac7c5570530d1ddca0901700
Jamshidkhuja Burikhujaev <j.burikhujaev@itransition.com>
2022-10-20 22:32:26 +0500

    feat: asdf

---
+++ .github/actions/issue/index.js
@@ -0,0 +1,24 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  const token = core.getInput('token');
+  const title = core.getInput('title ');
+  const body = core.getInput('body');
+  const assignees = core.getInput('assignees');
+
+  const octokit = github.getOctokit(token);
+
+  const response = octokit.rest.issues.create({
+    ...github.context.repo,
+    title,
+    body,
+    assignees: assignees ? assignees.split('\n') : undefined,
+  });
+
+  response.then((res) => {
+    core.setOutput(JSON.stringify(res.data));
+  });
+} catch (error) {
+  core.setFailed(error.message);
+}
